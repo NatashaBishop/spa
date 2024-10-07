fc6133564da9a215a23602a9bd8bd61116ff8b8b
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-10-07 17:31:18 +0100

    Update app.js

--- rtf-alex/client/scripts/app.js
+++ rtf-alex/client/scripts/app.js
@@ -4,6 +4,7 @@ createNabBar();
 function createNabBar(){
     const div = `
         <div>
+            <input type='button' value='posts' id='posts-button'>
             <input type='button' value='login' id='login-button'>
             <input type='button' value='register'>
         </div>  
@@ -18,6 +19,27 @@ function createNabBar(){
         .then(response => response.json())
         .then(data => {
             document.querySelector('#echo').textContent = data.Message;
+        })
+        .catch(err => console.log(err));
+    });
+
+    const postsButton = document.querySelector('#posts-button');
+    postsButton.addEventListener('click', () => {
+        fetch('http://localhost:8080/posts')
+        .then(response => response.json())
+        .then(arr => {
+            //document.querySelector('#echo').textContent = JSON.stringify(arr);
+            for(let i = 0; i < arr.length; i++) {
+                let post = arr[i];
+            
+                document.querySelector('#echo').textContent += JSON.stringify(post);
+               
+                console.log(post["Content"]);
+
+
+
+            }
+
         })
         .catch(err => console.log(err));
     });
